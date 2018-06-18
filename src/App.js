c256d82de2d4013f7aba2e1d5e4a92f06482a5c6
sammy.ha <sammy.ha@kingfisher.com>
2018-06-18 16:15:55 +0100

    adding Person to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() { // method to render to the screen
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Person />
       </div>
     );
   }
